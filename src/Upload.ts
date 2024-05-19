c081c9e526de5ee195741d10aebd82495320d103
Chris Yang <yy4382@outlook.com>
2024-05-19 14:52:35 +0800

    test

--- src/Upload.ts
+++ src/Upload.ts
@@ -142,21 +142,40 @@ export default class Uploader {
 			NOT_UP_TO_DATE,
 			UP_TO_DATE,
 		}
-
+		// try {
+		// 	await fs.promises.mkdir(dir, undefined);
+		// } catch (e) {
+		// 	// console.error(e);
+		// 	new Notice("Error while creating directory");
+		// 	return;
+		// }
+		// console.log(await fs.promises.readdir(dir));
 		let repoStat: RepoStat | undefined = undefined;
+
+		await git.clone({
+			fs,
+			http,
+			dir,
+			corsProxy: 'https://cors.isomorphic-git.org',
+			url: 'https://github.com/isomorphic-git/isomorphic-git',
+			ref: 'main',
+			singleBranch: true,
+			depth: 1,
+		});
+		console.log(await fs.promises.readdir(dir));		
 
 		new Notice("Start uploading to git, try using locally cached repo...");
 		// Check if the repo is already cloned and up to date
-		let localSha = "";
-		try {
-			localSha = await git.resolveRef({
-				fs,
-				dir: dir,
-				ref: "refs/heads/" + config.branch,
-			});
-		} catch {
-			repoStat = RepoStat.NOT_EXIST;
-		}
+		// let localSha = "";
+		// try {
+		// 	localSha = await git.resolveRef({
+		// 		fs,
+		// 		dir: dir,
+		// 		ref: "refs/heads/" + config.branch,
+		// 	});
+		// } catch {
+		// 	repoStat = RepoStat.NOT_EXIST;
+		// }
 
 		// if (repoStat === undefined) {
 		// 	// repo already exists. Check if the remote is up to date
