eb92e6521509d89e530d4bfc374e00ed86b28e4f
Chris Yang <yy4382@outlook.com>
2024-10-08 16:09:35 -0700

    refactor: remove unused

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
 import { Notice, Plugin, TFile } from "obsidian";
 import { SSSettings } from "@/type";
 import { DEFAULT_SETTINGS, Ob2StaticSettingTab } from "@/Settings";
-
-// import Uploader from "@/Upload";
 import { transform } from "./transform";
 
 import { gitUpload } from "@/upload/git";
@@ -13,21 +11,6 @@ export default class Ob2StaticPlugin extends Plugin {
 	async onload(): Promise<void> {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		// this.addRibbonIcon(
-		// 	"file-up",
-		// 	"Current file - Static Site MD Export",
-		// 	async () => {
-		// 		// Called when the user clicks the icon.
-		// 		new Notice("Starting process");
-		// 		const tFiles = [this.app.workspace.getActiveFile() as TFile];
-		// 		if (tFiles[0] === null) {
-		// 			new Notice("No file active");
-		// 			return;
-		// 		}
-		// 		await this.process(tFiles);
-		// 	},
-		// );
 		this.addRibbonIcon(
 			"file-up",
 			"Current file - Static Site MD Export",
@@ -45,65 +28,12 @@ export default class Ob2StaticPlugin extends Plugin {
 				await gitUpload(posts, this.settings.uploader.git);
 			},
 		);
-		// this.addRibbonIcon(
-		// 	"folder-up",
-		// 	"All validate files - Static Site MD Export",
-		// 	async () => {
-		// 		// Called when the user clicks the icon.
-		// 		new Notice("Starting process");
-		// 		const tFiles = this.app.vault.getFiles();
-		// 		await this.process(tFiles);
-		// 	},
-		// );
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new Ob2StaticSettingTab(this.app, this));
 	}
 
 	onunload(): void {}
-
-	/**
-	 * Processes the notes and uploads them to the specified S3 bucket.
-	 */
-	// async process(tFiles: TFile[]): Promise<void> {
-	// 	const uploader = new Uploader(this.app, this.settings);
-
-	// 	const validRe = await Promise.all(
-	// 		tFiles.map((post) => this.ValidatePost(post)),
-	// 	);
-	// 	const postsOb = validRe.filter((post) => post !== null) as Post[];
-	// 	if (postsOb.length === 0) {
-	// 		new Notice("No valid posts found");
-	// 		return;
-	// 	}
-	// 	const postHandler = new PostHandler(
-	// 		tFiles,
-	// 		postsOb,
-	// 		this.settings,
-	// 		this.app.vault,
-	// 	);
-	// 	const postsHexo = await postHandler.normalize();
-
-	// 	new Notice(`Process complete,\n Start uploading (${postsHexo.length})`);
-
-	// 	await uploader.upload(postsHexo);
-	// 	new Notice("Upload complete");
-	// }
-
-	// async ValidatePost(tFile: TFile): Promise<Post | null> {
-	// 	if (tFile.extension !== "md") return null;
-	// 	const noteContent = await this.app.vault.cachedRead(tFile);
-	// 	const frontmatter = this.app.metadataCache.getFileCache(tFile)
-	// 		?.frontmatter as Frontmatter;
-	// 	if (frontmatter?.published === true) {
-	// 		return {
-	// 			tFile: tFile,
-	// 			frontmatter: frontmatter,
-	// 			article: noteContent.split("---").slice(2).join("---"),
-	// 		};
-	// 	}
-	// 	return null;
-	// }
 
 	async loadSettings(): Promise<void> {
 		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
