476ab20e97ea80cf23719d691cab4acd03fa2daa
Chris Yang <yy4382@outlook.com>
2024-10-20 02:09:42 -0700

    2.0.0-beta.1

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -39,7 +39,7 @@ const context = await esbuild.context({
 	treeShaking: true,
 	outfile: "main.js",
 	inject: ["./polyfill-buffer.js"],
-	// minify: prod,
+	minify: prod,
 });
 
 if (prod) {
--- polyfill-buffer.js
+++ polyfill-buffer.js
@@ -1,10 +1,27 @@
-import { Platform } from 'obsidian';
+// https://github.com/Vinzent03/obsidian-git/blob/master/polyfill_buffer.js
+/*
+MIT License
+
+Copyright (c) 2020 Vinzent03, Denis Olehov
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+*/
+
+import { Platform } from "obsidian";
 let buffer;
 if (Platform.isMobileApp) {
-    // eslint-disable-next-line @typescript-eslint/no-require-imports
-    buffer = require('buffer/index.js').Buffer
+	// eslint-disable-next-line @typescript-eslint/no-require-imports
+	buffer = require("buffer/index.js").Buffer;
 } else {
-    buffer = global.Buffer
+	buffer = global.Buffer;
 }
 
 export const Buffer = buffer;
