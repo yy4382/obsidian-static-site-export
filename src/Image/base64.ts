ab4a2b1a7e98d301fcf310fc5e6ca71d6e9379f5
Chris Yang <yy4382@outlook.com>
2024-10-21 15:31:44 -0700

    fix: base64 image handler tag repeat

--- src/Image/base64.ts
+++ src/Image/base64.ts
@@ -1,43 +1,44 @@
-import { Reference, TFile } from "obsidian";
+import type { Reference, TFile } from "obsidian";
 import { ImageTransformer } from "./base";
 import type { Post, TransformCtx } from "@/type";
 
 export class Base64Transformer extends ImageTransformer {
-	fileMap: Map<string, [string, string, TFile]>;
+	fileMap: Map<string, string>;
+	contentMap: Map<string, [string, string]>;
 	constructor(ctx: TransformCtx) {
 		super(ctx);
-		this.fileMap = new Map<string, [string, string, TFile]>();
+		this.fileMap = new Map<string, string>();
+		this.contentMap = new Map<string, [string, string]>();
 	}
 	async onTransform(
 		link: Reference,
 		_sourceTFile: TFile,
 		targetTFile: TFile,
 	): Promise<string> {
-		if (this.fileMap.has(targetTFile.path)) {
-			const tagName = this.fileMap.get(targetTFile.path)![0];
-			return `![${link.displayText ?? link.link}][${tagName}]`;
+		let tagName = this.fileMap.get(targetTFile.path);
+		if (!tagName) {
+			tagName = `img${this.fileMap.size + 1}`;
+			this.fileMap.set(targetTFile.path, tagName);
 		}
-		const tagName = `img${this.fileMap.size + 1}`;
-		const arraybuffer = await this.ctx.readBinary(targetTFile);
 
-		// Convert arraybuffer to base64
-		const base64 = btoa(
-			new Uint8Array(arraybuffer).reduce(
-				(data, byte) => data + String.fromCharCode(byte),
-				"",
-			),
-		);
-
-		// Store the base64 data in the fileMap
-		this.fileMap.set(targetTFile.path, [tagName, base64, targetTFile]);
-
+		if (!this.contentMap.has(tagName)) {
+			const arraybuffer = await this.ctx.readBinary(targetTFile);
+			// Convert arraybuffer to base64
+			const base64 = btoa(
+				new Uint8Array(arraybuffer).reduce(
+					(data, byte) => data + String.fromCharCode(byte),
+					"",
+				),
+			);
+			this.contentMap.set(tagName, [base64, targetTFile.extension]);
+		}
 		// Return the tag name to be used as a placeholder
 		return `![${link.displayText ?? link.link}][${tagName}]`;
 	}
 	async onAfterTransform(post: Post): Promise<void> {
 		let appendContent = "\n\n";
-		this.fileMap.forEach(([tagName, base64, file]) => {
-			appendContent += `[${tagName}]: data:image/${file.extension};base64,${base64}\n`;
+		this.contentMap.forEach(([base64, ext], tagName) => {
+			appendContent += `[${tagName}]: data:image/${ext};base64,${base64}\n`;
 		});
 		post.content += appendContent;
 	}
